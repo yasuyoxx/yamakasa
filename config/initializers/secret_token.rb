6fd38ffcb775d8cb2db4432677819ed60c51d30b
yasuyo <yasuyo.kxoxo@gmail.com>
2015-06-20 06:29:34 +0000

    first step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yamakasa::Application.config.secret_key_base = 'a9f8831bd61066cff44d7fb79fc95c7f476133890d1f969ab81e9b939dc59833d4deeadaa906b488bfed4aec7bd1ae4e14eaeb512e5323c4f388aee300918339'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yamakasa::Application.config.session_store :cookie_store, key: '_yamakasa_session'
